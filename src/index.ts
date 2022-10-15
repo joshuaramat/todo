437d06296a47631d95d288d42d6fa2e8e2831483
Joshua Ramat <ramatjoshua@gmail.com>
2022-10-14 18:33:13 -0700

    resolved bug where list items were not rendering in the ul element

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-/* Import the v4 function from the uuid package. */
+/* Importing the v4 function from the uuid package. */
 import { v4 as uuidV4 } from "uuid"
 
 /**
- * Create Task type, an object with an id, title, completed, and createdAt property.
- * id - A unique identifier for the task.
- * title - The title of the task.
- * completed - A boolean value that indicates whether the task is completed or not.
- * createdAt - The date the task was created
+ * A Task is an object with an id, title, completed, and createdAt property.
+ * @property {string} id - A unique identifier for the task.
+ * @property {string} title - The title of the task.
+ * @property {boolean} completed - A boolean value that indicates whether the task is completed or not.
+ * @property {Date} createdAt - The date the task was created
  */
 type Task = {
   id: string
@@ -15,23 +15,23 @@ type Task = {
   createdAt: Date
 }
 
-/* Use TypeScript to type check the HTML elements. */
-const list = document.querySelector<HTMLUListElement>("#list");
-const form = document.getElementById("#new-task-form") as HTMLFormElement | null;
-const input = document.querySelector<HTMLInputElement>("new-task-title");
+/* Selecting the elements from the DOM. */
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
 
-/* This is an event listener that listens for the submit event on the form. 
-When the form is submitted,the event listener will prevent the default behavior of the form, 
-check if the input value is empty or null, and if it is, return undefined. 
-If the input value is not empty or null, it will create a new task object with the id, title, completed, and createdAt properties, 
-and then create a new list item and append it to the list. */
-form?.addEventListener("submit", e => {
-  e.preventDefault();
+/* Add an event listener to the form element that listens for the submit event. When the submit
+event is fired, it will prevent the default behavior of the form element, check if the input value
+is empty or null, and if it isn't, it will create a new task object with the id, title, completed,
+and createdAt properties. It will then create a new list item and append it to the list. */
+form?.addEventListener("submit", (e) => {
+  /* It prevents the default behavior of the form element. */
+  e.preventDefault()
   
-  /* Check if the input value is empty or null. If it is, return undefined. */
+  /* Checking if the input value is empty or null. If it is, it will return undefined. */
   if (input?.value == "" || input?.value == null) return
-  
-  /* Create a new task object with the id, title, completed, and createdAt properties. */
+
+  /* Creating a new task object with the id, title, completed, and createdAt properties. */
   const newTask: Task = {
     id: uuidV4(),
     title: input.value,
@@ -39,21 +39,23 @@ form?.addEventListener("submit", e => {
     createdAt: new Date()
   }
 
-  /* Create a new list item and appending it to the list. */
+  /* Creating a new list item and appending it to the list. */
   addListItem(newTask)
-  input.value = ""
 })
 
+/**
+ * addListItem() creates a new list item, label, and checkbox, sets the type attribute of the checkbox element to
+ * "checkbox", and appends the label and checkbox to the list item, which is then appended to the list
+ * @param {Task} task - Task - This is the parameter that we're passing into the function.
+ */
 function addListItem(task: Task) {
   /* Creating a new list item, label, and checkbox. */
   const item = document.createElement("li")
   const label = document.createElement("label")
   const checkbox = document.createElement("input")
 
-  /* Set the type of the checkbox to "checkbox". */
+  /* Setting the type attribute of the checkbox element to "checkbox". */
   checkbox.type = "checkbox"
-  /* Set the checked property of the checkbox to the completed property of the task. */
-  checkbox.checked = task.completed
 
   /* Creating a new list item, label, and checkbox. */
   label.append(checkbox, task.title)
