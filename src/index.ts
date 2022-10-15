edb49e3a4bccafe8348632f0dbb4fb02a215c4de
Joshua Ramat <ramatjoshua@gmail.com>
2022-10-14 18:10:13 -0700

    set up functions to add list items, however list items are not being appended to UL element as expected

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,62 @@
+/* Import the v4 function from the uuid package. */
+import { v4 as uuidV4 } from "uuid"
+
 /**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+ * Create Task type, an object with an id, title, completed, and createdAt property.
+ * id - A unique identifier for the task.
+ * title - The title of the task.
+ * completed - A boolean value that indicates whether the task is completed or not.
+ * createdAt - The date the task was created
+ */
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
+}
 
-import confetti from 'canvas-confetti';
+/* Use TypeScript to type check the HTML elements. */
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.getElementById("#new-task-form") as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>("new-task-title");
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+/* This is an event listener that listens for the submit event on the form. 
+When the form is submitted,the event listener will prevent the default behavior of the form, 
+check if the input value is empty or null, and if it is, return undefined. 
+If the input value is not empty or null, it will create a new task object with the id, title, completed, and createdAt properties, 
+and then create a new list item and append it to the list. */
+form?.addEventListener("submit", e => {
+  e.preventDefault();
+  
+  /* Check if the input value is empty or null. If it is, return undefined. */
+  if (input?.value == "" || input?.value == null) return
+  
+  /* Create a new task object with the id, title, completed, and createdAt properties. */
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+  /* Create a new list item and appending it to the list. */
+  addListItem(newTask)
+  input.value = ""
+})
+
+function addListItem(task: Task) {
+  /* Creating a new list item, label, and checkbox. */
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+
+  /* Set the type of the checkbox to "checkbox". */
+  checkbox.type = "checkbox"
+  /* Set the checked property of the checkbox to the completed property of the task. */
+  checkbox.checked = task.completed
+
+  /* Creating a new list item, label, and checkbox. */
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
